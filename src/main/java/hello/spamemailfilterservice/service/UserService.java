f1506e621340c4343d54292dfeaa18cab37b8e5b
coshin71 <mincolong@naver.com>
2022-05-14 20:06:54 +0900

    feat: password encode 추가

--- src/main/java/hello/spamemailfilterservice/service/UserService.java
+++ src/main/java/hello/spamemailfilterservice/service/UserService.java
@@ -4,6 +4,7 @@ import hello.spamemailfilterservice.dto.UserDto;
 import hello.spamemailfilterservice.model.User;
 import hello.spamemailfilterservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void signup(UserDto userDto) {
+        String rawPassword = userDto.getPassword();
+        userDto.setPassword(passwordEncoder.encode(rawPassword));
         User user = userDto.toEntity();
         userRepository.save(user);
     }
