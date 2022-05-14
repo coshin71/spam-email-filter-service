57ddafa5d2c5f8c7e3086357618ab629a7cbc8f8
coshin71 <mincolong@naver.com>
2022-05-14 17:46:06 +0900

    Initial commit

---
+++ src/main/java/hello/spamemailfilterservice/SpamEmailFilterServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.spamemailfilterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamEmailFilterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamEmailFilterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/spamemailfilterservice/SpamEmailFilterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spamemailfilterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamEmailFilterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
